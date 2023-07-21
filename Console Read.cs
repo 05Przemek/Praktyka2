ba28c5c7db6a9c9215b093d2e974a52f7af377f8
05Przemek <przemochmielowiec@gmail.com>
2023-07-21 08:31:35 +0200

    Escape

---
+++ Console Read.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Skip
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Podaj imie");
+			string Imie = Console.ReadLine();
+
+
+		}
+	}
+}
---
+++ Escape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Tytulksiązki = $"Polecam k\\siążkę {{0}} \ "Czysty kod\" Rob\erta C. Mar\ti\na";
+            Console.WriteLine(Tytulksiazki);
+        }
+	}
+}
