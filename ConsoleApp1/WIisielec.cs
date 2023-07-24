1d26a82296b149525fa971d54760d415e24d6d11
05Przemek <przemochmielowiec@gmail.com>
2023-07-24 15:47:14 +0200

    wisielec.3 (bez plików)

--- ConsoleApp1/WIisielec.cs
+++ ConsoleApp1/WIisielec.cs
@@ -64,3 +64,60 @@ namespace WisielecGame
       |
 ========="
             };
+
+            string[] words = { "wisielec", "komputer", "programowanie", "rozwojowiec", "python" };
+            Random random = new Random();
+            string wordToGuess = words[random.Next(words.Length)];
+            char[] guessedLetters = new char[wordToGuess.Length];
+            int attempts = 0;
+            bool isGameOver = false;
+
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                guessedLetters[i] = '_';
+            }
+
+            Console.WriteLine("Witaj w grze Wisielec!");
+            Console.WriteLine("Zgadnij słowo: " + new string(guessedLetters));
+
+            while (!isGameOver)
+            {
+                Console.WriteLine(hangmanImages[attempts]);
+
+                Console.Write("Podaj literę: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool isCorrectGuess = false;
+
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i] == input)
+                    {
+                        guessedLetters[i] = input;
+                        isCorrectGuess = true;
+                    }
+                }
+
+                if (!isCorrectGuess)
+                {
+                    attempts++;
+                }
+
+                Console.WriteLine("Zgadnięte litery: " + new string(guessedLetters));
+
+                if (Array.IndexOf(guessedLetters, '_') == -1)
+                {
+                    Console.WriteLine("Brawo! Odgadłeś słowo: " + wordToGuess);
+                    isGameOver = true;
+                }
+                else if (attempts >= hangmanImages.Length)
+                {
+                    Console.WriteLine("Przegrałeś! Nie udało Ci się odgadnąć słowa.");
+                    Console.WriteLine("Poprawne słowo to: " + wordToGuess);
+                    isGameOver = true;
+                }
+            }
+        }
+    }
+}
